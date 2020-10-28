905e46004b4f9e006302ce98c12a930d5aa404cb
Claudio Coronel Milla <clau_coronelmilla@hotmail.com>
2020-10-28 10:44:47 -0300

    ....

--- index.php
+++ index.php
@@ -11,12 +11,13 @@
   <!-- Header -->
   <header class="masthead d-flex">
     <div class="container text-center my-auto">
-      <!--<img src="assets/img/cuariblanco.png" width="300px"> -->
-      <!--<h1 class="mb-1">Compro en mi Pueblo</h1> -->
-      <!--<h3 class="mb-5">
-        <em style="color: white;">Una forma diferente de comprar</em>
+      <img src="assets/img/mmp.png" width="300px">
+      <br><br><br>
+      <h3 class="mb-1" style="color: white"> Solicitud de nuevos desarrollos</h3>
+      <h3 class="mb-5">
+        <!-- <em style="color: white;">Una forma diferente de comprar</em>-->
       </h3>
-      <a class="btn btn-success btn-sm js-scroll-trigger" href="#about">Accedé al programa</a> -->
+      <a class="btn btn-success btn-sm js-scroll-trigger" href="#about">Acceder</a>
     </div>
     <div class="overlay"></div>
   </header>
@@ -36,51 +37,50 @@
             
             <div class="alert ">
               <div class="alert alert-success" role="alert">
-                <h1>Estos son los datos solicitados para el desarrollo de una nueva aplicacion y/o sistema</h1>
-                <p>...</p>
-                <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>                
+                <h4>A continuación le vamos a solicitar los datos requeridos para realizar el sistema y/o aplicación que deseas.</h4>
+                <p><a class="btn btn-primary" href="#" role="button">Continuar</a></p>                
               </div>
               <br>
               <div class="form-group">
-                <label>Area del Proyecto: </label>
-                <input type="text" class="form-control" id="apellido" name="apellido" required>
+                <label>Área que solicita el proyecto: </label>
+                <input type="text" class="form-control" id="area" name="area" required>
               </div>
               <div class="form-group">
                 <label>Responsable: </label>
-                <input type="text" class="form-control" id="apellido" name="apellido" required>
+                <input type="text" class="form-control" id="responsable" name="responsable" required>
               </div> 
               <div class="form-group">
                 <label>Correo Electrónico: </label>
-                <input type="text" class="form-control" id="apellido" name="apellido" required>
+                <input type="text" class="form-control" id="mail" name="mail" required>
               </div> 
               <div class="form-group">
                 <label>Número de teléfono: </label>
-                <input type="text" class="form-control" id="apellido" name="apellido" required>
+                <input type="text" class="form-control" id="telefono" name="telefono" required>
               </div> 
               <div class="form-group">
-                <label>Fecha estimada de su implementacion y/o lanzamiento: </label>
-                <input type="text" class="form-control" id="apellido" name="apellido" required>
+                <label>Fecha estimada de su implementación y/o lanzamiento: </label>
+                <input type="text" class="form-control" id="fechaimplementacion" name="fechaimplementacion" required>
               </div>                             
               <div class="form-group">
                 <label>Tipo de aplicación: </label>
                 <select class="form-control" id="tipoapp" name="tipoapp">
                   <option>Seleccionar</option>
-                  <option>Aplicacion de gestion</option>
-                  <option>Comercio Electronico</option>
-                  <option>Comunicacion interna</option>
-                  <option>Mensajeria instantanea</option>
-                  <option>Fidelizacion de Usuarios</option>
+                  <option>Aplicación de Gestión</option>
+                  <option>Comercio Electrónico</option>
+                  <option>Comunicación interna</option>
+                  <option>Mensajería Instantánea</option>
+                  <option>Fidelización de Usuarios</option>
                   <option>Servicio a clientes</option>
                 </select>
               </div>
               <div class="form-group">
                 <label>Nombre de la aplicación</label>
-                <input type="text" class="form-control" id="documento" name="documento" required>
+                <input type="text" class="form-control" id="nombreapp" name="nombreapp" required>
               </div>
 
               <div class="form-group">
-                <label>Breve descripcion del proyecto:</label>
-                <textarea class="form-control"></textarea>
+                <label>Breve descripción del proyecto:</label>
+                <textarea class="form-control" id="descripcionapp" name="descripcionapp"></textarea>
               </div>
             </div>
 
@@ -89,8 +89,8 @@
               <h4>Algunos datos más</h4>
               <br>
               <div class="form-group">
-                <label>Los usuarios podran almacenar datis en su aplicacion? </label>
-                <select class="form-control" id="tipoapp" name="tipoapp">
+                <label>Los usuarios podrán almacenar datos en su aplicación? </label>
+                <select class="form-control" id="almacenainfo" name="almacenainfo">
                   <option>Seleccionar</option>
                   <option value="si">Si</option>
                   <option value="no">No</option>
@@ -98,8 +98,8 @@
                 </select>
               </div>
               <div class="form-group">
-                <label>Los usuarios recibiran notificaciones? </label>
-                <select class="form-control" id="tipoapp" name="tipoapp">
+                <label>Los usuarios recibirán notificaciones? </label>
+                <select class="form-control" id="recibenotificacion" name="recibenotificacion">
                   <option>Seleccionar</option>
                   <option value="si">Si</option>
                   <option value="no">No</option>
@@ -107,8 +107,8 @@
                 </select>
               </div>
               <div class="form-group">
-                <label>Que nivel de desarrollo estas buscando? </label>
-                <select class="form-control" id="tipoapp" name="tipoapp">
+                <label>Que nivel de desarrollo estás buscando? </label>
+                <select class="form-control" id="nivelapp" name="nivelapp">
                   <option>Seleccionar</option>
                   <option value="si">Baja</option>
                   <option value="no">Media</option>
@@ -116,8 +116,8 @@
                 </select>
               </div>
               <div class="form-group">
-                <label>Tu aplicacion debe estar integrado con otro sitio web y/o servicio municipal? </label>
-                <select class="form-control" id="tipoapp" name="tipoapp">
+                <label>Tu aplicación debe estar integrado con otro sitio web y/o servicio municipal? </label>
+                <select class="form-control" id="integrarapp" name="integrarapp">
                   <option>Seleccionar</option>
                   <option value="si">Si</option>
                   <option value="no">No</option>
@@ -125,8 +125,8 @@
                 </select>
               </div>
               <div class="form-group">
-                <label>Los usuarios deben tener sus propios perfiles y poder administrar su propia informacion? </label>
-                <select class="form-control" id="tipoapp" name="tipoapp">
+                <label>Los usuarios deben tener sus propios perfiles y poder administrar su propia información? </label>
+                <select class="form-control" id="perfilesapp" name="perfilesapp">
                   <option>Seleccionar</option>
                   <option value="si">Si</option>
                   <option value="no">No</option>
@@ -135,8 +135,8 @@
               </div>
 
               <div class="form-group">
-                <label>Tu aplicacion necesita un panel de administracion para cargar datos y/o visualizar informacion? </label>
-                <select class="form-control" id="tipoapp" name="tipoapp">
+                <label>Tu aplicación necesita un panel de administración para cargar datos y/o visualizar información? </label>
+                <select class="form-control" id="panelapp" name="panelapp">
                   <option>Seleccionar</option>
                   <option value="si">Si</option>
                   <option value="no">No</option>
@@ -145,18 +145,18 @@
               </div>
               
               <div class="form-group">
-                <label>Defina el esquema de colores de su sitio o aplicacion? </label>
-                <textarea class="form-control"></textarea>
+                <label>Defina el esquema de colores de su sitio o aplicación? </label>
+                <textarea class="form-control" id="esquemacolores" name="esquemacolores"></textarea>
               </div>
               
               <div class="form-group">
-                <label>Su sitio o aplicacion necesitara un menu?. Describa cuales son los elementos del menu en caso de necesitarlo </label>
-                <textarea class="form-control"></textarea>
+                <label>Su sitio o aplicación necesitará un menú?. Describa cuáles son los elementos del menú en caso de necesitarlo </label>
+                <textarea class="form-control" id="elementosmenu" name="elementosmenu"></textarea>
               </div>
 
               <div class="form-group">
-                <label>Que datos le solicitará a los visitantes de su sitio web o aplicacion? </label>
-                <textarea class="form-control"></textarea>
+                <label>Que datos le solicitará a los visitantes de su sitio web o aplicación? </label>
+                <textarea class="form-control" id="datossolicitados" name="datossolicitados"></textarea>
               </div>
             </div>
 
